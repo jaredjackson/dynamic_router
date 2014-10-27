69ebca08fad40b9acff7cb1bd70484853cb2d30a
Gustavo Berdugo <gustavo@coyo.com.br>
2014-10-27 11:29:31 -0200

    Better error handling.

--- lib/dynamic_router.rb
+++ lib/dynamic_router.rb
@@ -8,20 +8,25 @@ module DynamicRouter
       if ActiveRecord::Base.connection.table_exists? klass.table_name
         Rails.application.routes.draw do
           klass.find_each do |model|
-            unless options[:defaults].blank?
-              defaults = {}
+            begin
+              unless options[:defaults].blank?
+                defaults = {}
+                
+                options[:defaults].each do |key, value|
+                  defaults[key.to_sym] = value.call(model)
+                end  
+              end
               
-              options[:defaults].each do |key, value|
-                defaults[key.to_sym] = value.call(model)
-              end  
-            end
-            
-            _url = url.call(model).to_s
-            _target = target.to_s
-            
-            unless _url.blank? || _url.gsub(/\//, "").blank?
-              #puts "Routing #{route_method.to_s.upcase} #{_url}, to #{target}, defaults : #{defaults}"
-              send(route_method, _url, :to => _target, :defaults => defaults)
+              _url = url.call(model).to_s
+              _target = target.to_s
+              
+              unless _url.blank? || _url.gsub(/\//, "").blank?
+                #puts "[DEBUG] Routing #{route_method.to_s.upcase} #{_url}, to #{target}, defaults: #{defaults}"
+                #Rails.logger.info "[INFO] Routing #{route_method.to_s.upcase} #{_url}, to #{target}, defaults: #{defaults}"
+                send(route_method, _url, :to => _target, :defaults => defaults)
+              end
+            rescue Exception => e
+              Rails.logger.error "Error => #{e.class} - #{e.message}"
             end
           end   
         end
